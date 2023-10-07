450dbd3e01fc556c0651761057c84bdb17a83099
Vitor L. <vitoribeiro0024@gmail.com>
2023-10-06 22:38:05 -0300

    Correção de método

--- interface/demo/src/test/java/com/ordenador/TestMain.java
+++ interface/demo/src/test/java/com/ordenador/TestMain.java
@@ -1,6 +1,14 @@
 package com.ordenador;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import main.java.com.ordenador.Estudante;
+import main.java.com.ordenador.EstudantePorFrequenciaComparator;
+import main.java.com.ordenador.EstudantePorIdadeComparator;
+import main.java.com.ordenador.EstudantePorNomeComparator;
+import main.java.com.ordenador.EstudantePorNotasComparator;
+import main.java.com.ordenador.EstudantePorSobrenomeComparator;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -66,5 +74,13 @@ class TestMain {
         assertEquals("João", estudantes.get(2).getNome());
         assertEquals("Ana", estudantes.get(3).getNome());
         assertEquals("Maria", estudantes.get(4).getNome());
+    }
+
+    public static void seedList(List<Estudante> list) {
+        list.add(new Estudante("João", "Silva", 20, new double[] { 8.5, 7.2, 9.0 }, 0.90f));
+        list.add(new Estudante("Maria", "Santos", 22, new double[] { 7.0, 7.5, 8.0 }, 0.85f));
+        list.add(new Estudante("Pedro", "Ferreira", 19, new double[] { 9.5, 8.0, 7.5 }, 0.92f));
+        list.add(new Estudante("Ana", "Oliveira", 21, new double[] { 8.0, 8.5, 7.2 }, 0.88f));
+        list.add(new Estudante("Mariana", "Lima", 23, new double[] { 7.8, 8.2, 8.5 }, 0.91f));
     }
 }
